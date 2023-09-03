7364ea81e81c59dc994c299320c3b73b2285a37e
Malte Dostal <melophobic@maltes-imac.speedport.ip>
2023-09-04 01:21:18 +0200

    mkdir -p target directories in build scripts

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ set -xe
 
 GCC_FLAGS="--std=c99 -Wall -Wextra -Wpedantic"
 
+mkdir -p target/static
+
 gcc -c $GCC_FLAGS -o target/gifmethat.o src/gifmethat.c
 ar rcs target/static/libgifmethat.a target/gifmethat.o
